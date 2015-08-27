ea895999e98bd1aa272b29dbbd324d37ab4f8daa
Marcus Barnes <mavjbarnes@gmail.com>
2015-08-27 11:03:12 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// Grabs the id's for digits
+var hour = document.getElementById('hours'),
+    minutes = document.getElementById('minutes'),
+    seconds = document.getElementById('seconds'),
+    miliseconds = document.getElementById('miliseconds'),
+
+// Grabs the id's for the buttons
+    startButton = document.getElementById('startButton'),
+    pauseButton =  document.getElementById('pauseButton'),
+    stopButton = document.getElementById('stopButton'),
+    resetButton = document.getElementById('resetbutton');

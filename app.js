aab275f4998b9e08430d5c2c56f2ad2841b19400
Marcus Barnes <mavjbarnes@gmail.com>
2015-08-27 11:40:49 -0400

    got miliseconds working

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Globals
 // Grabs the id's for digits
 var hour = document.getElementById('hours'),
     minutes = document.getElementById('minutes'),
@@ -8,4 +9,32 @@ var hour = document.getElementById('hours'),
     startButton = document.getElementById('startButton'),
     pauseButton =  document.getElementById('pauseButton'),
     stopButton = document.getElementById('stopButton'),
-    resetButton = document.getElementById('resetbutton');
+    resetButton = document.getElementById('resetbutton'),
+
+    setTime,
+    currentTime,
+    difference,
+    timer = 0;
+
+// Gets current time and sets the interval
+var start = function() {
+  setTime = Date.now();
+  setInterval(update, 100);
+};
+
+// Updates the current time
+var update = function() {
+  currentTime = Date.now();
+  difference = currentTime - setTime;
+  timer += difference;
+
+  updateScreen();
+  setTime = currentTime;
+};
+
+// Converts the current time to miliseconds 
+var updateScreen = function() {
+  miliseconds.innerText = timer/1000;
+};
+
+startButton.addEventListener('click', start);
